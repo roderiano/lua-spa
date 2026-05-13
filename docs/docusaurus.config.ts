9fb89f8e20eba54dbd6b4dedf19c2c16b2569d37
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2026-05-13 14:57:37 -0300

    chore(docs) update docs images

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: ' SPA Documentation',
+  title: 'MOON SPA Documentation',
   tagline: 'Python Framework for Single Page Applications',
   favicon: 'img/favicon.svg',
 
